965598a6580e67b9099ea5d6029262fb214bb3c3
omgbqh98 <omgbqh@gmai.com>
2023-08-21 21:51:06 +0700

    header,search

---
+++ src/components/Ticket.vue
@@ -0,0 +1,145 @@
+<script setup>
+</script>
+
+<template>
+    <div class="MuiBox-root jss3306 jss3289 snipcss-qkjOk style-raH7T" id="style-raH7T">
+        <div class="MuiBox-root jss3307 jss3294">
+            <div class="MuiBox-root jss3308 jss3297">
+                <div class="MuiBox-root jss3309 jss3292">
+                    Từ
+                </div>
+                <div class="MuiBox-root jss3310 style-o9ml3" id="style-o9ml3">
+                    <div class="jss3303 style-wbvN8" id="style-wbvN8">
+                        <strong>
+                            SGN
+                        </strong>
+                        (Hồ Chí Minh)
+                    </div>
+                    <div class="MuiInputBase-root MuiInput-root jss3290 style-4pfdW" id="style-4pfdW">
+                        <input autocomplete="off" name="departure" placeholder="Thành phố, sân bay" type="text"
+                            class="MuiInputBase-input MuiInput-input" value="SGN (Hồ Chí Minh)">
+                    </div>
+                    <div class="jss3296 style-jfPJf" id="style-jfPJf">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="MuiBox-root jss3311 jss3291">
+            <svg width="44" height="44" fill="none" style="height: 32px; cursor: pointer;">
+                <g filter="url(#switch_svg__filter0_d)">
+                    <rect x="38" width="32" height="32" rx="16" transform="rotate(90 38 0)" fill="#00B6F3">
+                    </rect>
+                </g>
+                <path d="M28.667 11.833h-10 10z" fill="#fff">
+                </path>
+                <path d="M28.667 11.833L25.333 8.5v6.667l3.334-3.334zm0 0h-10" stroke="#fff" stroke-width="2.1"
+                    stroke-linecap="round" stroke-linejoin="round">
+                </path>
+                <path d="M15.333 20.167h10-10z" fill="#fff">
+                </path>
+                <path d="M15.333 20.167l3.334 3.333v-6.667l-3.334 3.334zm0 0h10" stroke="#fff" stroke-width="2.1"
+                    stroke-linecap="round" stroke-linejoin="round">
+                </path>
+                <defs>
+                    <filter id="switch_svg__filter0_d" x="0" y="0" width="44" height="44" filterUnits="userSpaceOnUse"
+                        color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix">
+                        </feFlood>
+                        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0">
+                        </feColorMatrix>
+                        <feOffset dy="6">
+                        </feOffset>
+                        <feGaussianBlur stdDeviation="3">
+                        </feGaussianBlur>
+                        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0.713726 0 0 0 0 0.952941 0 0 0 0.2 0">
+                        </feColorMatrix>
+                        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow">
+                        </feBlend>
+                        <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape">
+                        </feBlend>
+                    </filter>
+                </defs>
+            </svg>
+        </div>
+        <div class="MuiBox-root jss3312">
+            <div class="MuiBox-root jss3313 jss3297">
+                <div class="MuiBox-root jss3314 jss3292">
+                    Đến
+                </div>
+                <div class="MuiBox-root jss3315 style-YqWaH" id="style-YqWaH">
+                    <div class="jss3303 style-KH2Ak" id="style-KH2Ak">
+                    </div>
+                    <div class="MuiInputBase-root MuiInput-root jss3290 style-iH7o4" id="style-iH7o4">
+                        <input autocomplete="off" name="destination" placeholder="Thành phố, sân bay" type="text"
+                            class="MuiInputBase-input MuiInput-input" value="">
+                    </div>
+                    <div class="jss3296 style-sUxPO" id="style-sUxPO">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="MuiBox-root jss3316 jss3295">
+            <div class="MuiBox-root jss3317 style-Hgs7M" id="style-Hgs7M">
+                <div class="MuiBox-root jss3318 jss3292">
+                    Ngày đi
+                    <div class="MuiBox-root jss3319 jss3293">
+                        T2, 21 tháng 8
+                    </div>
+                    <div class="jss3296 style-Esjez" id="style-Esjez">
+                    </div>
+                </div>
+                <div class="MuiBox-root jss3320 jss3292 style-5RfRn" id="style-5RfRn">
+                    Ngày về
+                    <span class="MuiSwitch-root jss3321">
+                        <span
+                            class="MuiButtonBase-root MuiIconButton-root jss3323 MuiSwitch-switchBase MuiSwitch-colorPrimary"
+                            aria-disabled="false">
+                            <span class="MuiIconButton-label">
+                                <input class="jss3326 MuiSwitch-input" name="checkedA" type="checkbox"
+                                    aria-label="secondary checkbox" value="">
+                                <span class="MuiSwitch-thumb">
+                                </span>
+                            </span>
+                            <span class="MuiTouchRipple-root">
+                            </span>
+                        </span>
+                        <span class="MuiSwitch-track">
+                        </span>
+                    </span>
+                    <div class="MuiBox-root jss3327 jss3293">
+                        <div class="jss3301">
+                            Chọn ngày về
+                        </div>
+                    </div>
+                    <div class="jss3296 style-D1Qpy" id="style-D1Qpy">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="MuiBox-root jss3328 jss3295">
+            <div class="MuiBox-root jss3329 style-4BEfB" id="style-4BEfB">
+                <div class="MuiBox-root jss3330 jss3297">
+                    <div class="MuiBox-root jss3331 jss3292">
+                        Số khách, hạng ghế
+                    </div>
+                    <div class="MuiBox-root jss3332 jss3293 style-7LS8o" id="style-7LS8o">
+                        1 khách, phổ thông, phổ thông đặc biệt
+                    </div>
+                    <div class="jss3296 style-jYWVb" id="style-jYWVb">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedSecondary style-WJwY2"
+            tabindex="0" type="button" id="style-WJwY2">
+            <span class="MuiButton-label">
+                <svg width="23" height="24" fill="none" class="svgFillAll" style="stroke: white;">
+                    <path d="M21.312 22.5l-4.742-4.742m2.71-7.451a8.806 8.806 0 11-17.613 0 8.806 8.806 0 0117.613 0z"
+                        stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+            </svg>
+        </span>
+        <span class="MuiTouchRipple-root">
+        </span>
+    </button>
+</div></template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Hotel from './components/Hotel.vue';
+import Ticket from './components/Ticket.vue';
+import HomeStay from './components/HomeStay.vue';
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: Hotel },
+    { path: '/ticket', component: Ticket },
+    { path: '/homeStay', component: HomeStay }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
